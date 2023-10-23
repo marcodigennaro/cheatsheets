2ecb278a39c8606b8bf4e3b616a2a2ea974d8d21
marcodigennaro <m.di.gennaro@outlook.com>
2023-10-23 16:28:32 +0200

    pgel online

--- pip_install_pgel.sh
+++ pip_install_pgel.sh
@@ -10,30 +10,4 @@
 # conda activate pgel
 # conda install conda=23.9.0
 
-pip_packages='
-    jupyterlab
-    nbclassic'
-
-for package in `echo $pip_packages`; do
-    echo ===============  pip install $package
-    pip install $package
-    echo ---------------  DONE
-    done
-
-conda_packages='
-    ase
-    pymatgen
-    nglview
-    asap3
-    gpaw
-    openmpi
-    tblite
-    tblite-python
-    xtb-python
-    '
-
-for package in `echo $conda_packages`; do
-  echo ===============  conda install $package
-  conda install -y -c conda-forge $package
-  echo ---------------  DONE
-  done
+conda install -y -c conda-forge ase pymatgen nglview asap3 gpaw openmpi tblite tblite-python xtb-python jupyterlab
